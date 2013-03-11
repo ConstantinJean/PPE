57b4c2bae1967f5c9bea3f695ac81ad7194e84ea
Lyokoi <lyokoi@orange.fr>
2013-03-11 22:19:22 +0100

    add suppression (+more in english with grammar fault for Kustomkadiac

--- src/Musee/CollectionBundle/Controller/CollectionController.php
+++ src/Musee/CollectionBundle/Controller/CollectionController.php
@@ -69,4 +69,35 @@ class CollectionController extends Controller
 		array('form' => $form->createView(),
 		'objet' => $objet));
 	}
+	
+	public function supprimerObjetAction(Objet $objet)
+    {
+		// On crée un formulaire vide, qui ne contiendra que le champ CSRF
+		// Cela permet de protéger la suppression d'article contre cette faille
+		$form = $this->createFormBuilder()->getForm();
+
+		$request = $this->getRequest();
+		if ($request->getMethod() == 'POST') {
+		  $form->bind($request);
+
+		  if ($form->isValid()) {
+			// On supprime l'article
+			$em = $this->getDoctrine()->getManager();
+			$em->remove($objet);
+			$em->flush();
+
+			// On définit un message flash
+			//$this->get('session')->getFlashBag()->add('info', 'Article bien supprimé');
+
+			// Puis on redirige vers l'accueil
+			return $this->redirect($this->generateUrl('musee_accueil'));
+		  }
+		}
+
+		// Si la requête est en GET, on affiche une page de confirmation avant de supprimer
+		return $this->render('MuseeCollectionBundle:Collection:supprimer.html.twig', array(
+		  'objet' => $objet,
+		  'form'    => $form->createView()
+		));
+	}
 }
